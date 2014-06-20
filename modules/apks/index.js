3cb9d82467ba4b2b3b4f90436de3e77734ae289f
YinYong <yy15151877621@126.com>
2014-06-20 20:09:24 +0800

    [Update] download apk

--- modules/apks/index.js
+++ modules/apks/index.js
@@ -12,13 +12,30 @@ var formidable = require('formidable'),
 	
 exports.registerRoute = function(router) {
 
-    // as find all
-    router.get('/privacy/apks/', function(req, res, extras){
+	router.get('/privacy/apks/', function(req, res, extras){
         console.log('get privacy_apks url = ' + req.url);
         simpleResponse.printHeaders(req.headers, 'get privacy_apks');
 		
-		res.setHeader('Content-disposition', 'attachment; filename=' + 'privacy_app.apk');
-		comfunction.sendFile('/mnt/apks/privacy_app.apk', res);
+		res.setHeader('Content-disposition', 'attachment; filename=' + 'TZST.apk');
+		res.setHeader('Content-Disposition', 'attachment; filename=' + 'TZST.apk');
+		res.setHeader('content-disposition', 'attachment; filename=' + 'TZST.apk');
+		comfunction.sendFile('/mnt/apks/TZST.apk', res);
+    });
+	
+    router.get('/privacy/apks/TZST.apk', function(req, res, extras){
+        console.log('get privacy_apks url = ' + req.url);
+        simpleResponse.printHeaders(req.headers, 'get privacy_apks');
+		
+		res.setHeader('Content-disposition', 'attachment; filename=' + 'TZST.apk');
+		comfunction.sendFile('/mnt/apks/TZST.apk', res);
+    });
+	
+    router.get('/privacy/apks/LRZZ.apk', function(req, res, extras){
+        console.log('get privacy_apks url = ' + req.url);
+        simpleResponse.printHeaders(req.headers, 'get privacy_apks');
+		
+		res.setHeader('Content-disposition', 'attachment; filename=' + 'LRZZ.apk');
+		comfunction.sendFile('/mnt/apks/LRZZ.apk', res);
     });
 
 };
