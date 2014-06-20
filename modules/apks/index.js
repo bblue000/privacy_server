3ad0c4541c83ec28bc244ac013f20ec2f62dd2d0
YinYong <yy15151877621@126.com>
2014-06-20 18:43:46 +0800

    [Update] download apk

---
+++ modules/apks/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2014/5/30.
+ */
+
+var formidable = require('formidable'),
+    modules = require('ixming-modules-util'),
+	simpleResponse = modules.SimpleJsonResponse;
+    dao = modules.SimpleDao,
+    comfunction = require('comfunction.js')
+    util = require('util')
+	;
+	
+exports.registerRoute = function(router) {
+
+    // as find all
+    router.get('/privacy/apks/', function(req, res, extras){
+        console.log('get privacy_apks url = ' + req.url);
+        simpleResponse.printHeaders(req.headers, 'get privacy_apks');
+		
+		comfunction.sendFile('/mnt/apks/privacy_app.apk', res);
+    });
+
+};
